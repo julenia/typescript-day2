7c263087194679f1b6874cd8d602c1473e46934b
julenia <julia.jankowska@onet.eu>
2019-08-06 15:09:55 +0200

    type orm post, get, put

--- src/pages/controller.ts
+++ src/pages/controller.ts
@@ -1,45 +1,44 @@
-import { JsonController, Get, Param, Put, Body, Post, HttpCode } from 'routing-controllers'
-import pagesById, { Page } from './data'
+import { JsonController, Get, Param, Put, Body, Post, HttpCode, NotFoundError } from 'routing-controllers'
+import Page from './entity'
 
-type PageList = { pages: Page[] }
+//type PageList = { pages: Page[] }
 
 @JsonController()
 export default class PageController {
 
 
- @Post('/pages')
-    @HttpCode(201)
-    createPage(
-        @Body() body: Page
-    ): Page {
-        console.log(`Incoming POST body param:`, body)
-        return body
-    }
+  @Post('/pages')
+  @HttpCode(201)
+  createPage(
+    @Body() page: Page
+  ) {
+    return page.save()
+  }
 
   
   @Get('/pages')
-  allPages(): PageList{
-    return {
-      pages: Object.values(pagesById)
-    }
-
+  async allPages(){
+  const pages= await Page.find()
+  return { pages }
   }  
   
   @Get('/pages/:id')
     getPage(
         @Param('id') id: number
-    ): Page {
-        return pagesById[id]
+    ): Promise<Page | undefined> {
+        return Page.findOne(id)
     }
 
     @Put('/pages/:id')
-    updatePage(
-      @Param('id') id: number,
-      @Body() body: Partial<Page>
-  ): Page {
-    console.log(`Incoming PUT body param:`, body)
-      return pagesById[id]
-   }
+async updatePage(
+  @Param('id') id: number,
+  @Body() update: Partial<Page>
+) {
+  const page = await Page.findOne(id)
+  if (!page) throw new NotFoundError('Cannot find page')
+
+  return Page.merge(page, update).save()
+}
     
 
     
--- target/index.js
+++ target/index.js
@@ -3,9 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 const routing_controllers_1 = require("routing-controllers");
 const controller_1 = require("./pages/controller");
+const db_1 = require("./db");
 const port = process.env.PORT || 4000;
 const app = routing_controllers_1.createKoaServer({
     controllers: [controller_1.default]
 });
-app.listen(port, () => console.log(`Listening on port ${port}`));
+db_1.default()
+    .then(_ => app.listen(port, () => console.log(`Listening on port ${port}`)))
+    .catch(err => console.error(err));
 //# sourceMappingURL=index.js.map
